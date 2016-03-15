9f09f44e6f8e4db8c219f8c65868715fe8442d8d
Buck Baskin <mobile.wbaskin@gmail.com>
2016-03-15 11:16:17 -0400

    Outline bearify_core

--- src/bearify_core.py
+++ src/bearify_core.py
@@ -1,7 +1,18 @@
 Reading = namedtuple('Reading', ['bearing', 'size', 'r', 'g', 'b', 'a'])
 
-def one_click_process(opencv_image):
-    # TODO(buckbaskin): do something smart here
+def unwrap_image(donut):
+    # TODO(buckbaskin): take a donut image and convert it to an unwrapped 360
+    # degree image
+    return donut
+
+def panorama_to_readings(panorama):
+    # TODO(buckbaskin): given a panorama, find blobs using opencv and convert
+    #   their position in the image to a reading (bearing, size, color)
     list_of_readings = []
     list_of_readings.append(Reading())
     return list_of_readings
+
+def one_click_process(opencv_image):
+    wrapped = opencv_image
+    unwrapped = unwrap_image(wrapped)
+    return panorama_to_readings(unwrapped)
